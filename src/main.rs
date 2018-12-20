57be3a7694f96a80c181758c523dd8a45a6c598f
Otavio Salvador <otavio@ossystems.com.br>
2018-12-20 21:42:50 -0200

    main: Add command line parsing
    This makes use of `structopt` and `exitfailure` crates to avoid
    boilerplate code for command line processing.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,24 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-fn main() {
-    println!("Hello, world!");
+use exitfailure::ExitFailure;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[structopt(
+    name = "sendgrid-amqp-bridge",
+    author = "O.S. Systems Software LTDA. <contact@ossystems.com.br>",
+    about = "A SendGrid AMQP Bridge."
+)]
+#[derive(StructOpt, Debug)]
+struct Cli {
+    /// Configuration file to use
+    #[structopt(short = "c", long = "config")]
+    config: PathBuf,
+}
+
+fn main() -> Result<(), ExitFailure> {
+    let _cli = Cli::from_args();
+
+    Ok(())
 }
