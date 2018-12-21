26f63c9d62bb5ad80a81d5314c82a882ab619dc8
Otavio Salvador <otavio@ossystems.com.br>
2018-12-20 22:02:32 -0200

    main: Add initial structured logging support
    This adds the `slog` crate to enable structured logging since the
    beginning of the project.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use exitfailure::ExitFailure;
+use slog::{info, o, Drain};
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -16,10 +17,29 @@ struct Cli {
     /// Configuration file to use
     #[structopt(short = "c", long = "config")]
     config: PathBuf,
+    /// Increase the verboseness level
+    #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
+    verbose: usize,
 }
 
 fn main() -> Result<(), ExitFailure> {
-    let _cli = Cli::from_args();
+    let cli = Cli::from_args();
+    let logger = init_logger(cli.verbose);
+
+    info!(logger, "starting...");
 
     Ok(())
+}
+
+fn init_logger(verbosity: usize) -> slog::Logger {
+    let drain = slog_term::term_compact()
+        .filter_level(match verbosity {
+            1 => slog::Level::Info,
+            2 => slog::Level::Debug,
+            _ => slog::Level::Trace,
+        })
+        .fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    slog::Logger::root(drain, o!())
 }
