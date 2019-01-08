669bd0f072ce7786c03371565602ac2d607950d2
Otavio Salvador <otavio@ossystems.com.br>
2019-01-07 23:35:18 -0200

    config: Remove debugging code
    This reduces the code and avoid mixing configuration loading and
    processing of the information.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/config.rs
+++ src/config.rs
@@ -5,7 +5,7 @@
 use crate::{amqp::AMQP, sendgrid::SendGrid};
 use failure::ResultExt;
 use serde::Deserialize;
-use slog::{debug, trace};
+use slog::debug;
 use std::{fs::File, io::Read, path::Path};
 
 #[derive(Deserialize)]
@@ -18,33 +18,14 @@ impl Config {
     /// Load configuration from filesystem. The file is expected to be
     /// YAML compatible.
     pub(crate) fn load(path: &Path, logger: &slog::Logger) -> Result<Self, failure::Error> {
+        debug!(logger, "Loading configuration file {:?}", &path);
+
         let mut buf = String::new();
         File::open(path)
             .context(format!("opening configuration file {:?}", &path))?
             .read_to_string(&mut buf)
             .context("reading configuration file")?;
 
-        let config: Self = serde_yaml::from_str(&buf).context("parsing the YAML configuration")?;
-
-        debug!(
-            logger,
-            "loaded configuration file";
-            "templates" => format!("{:?}",
-            config.sendgrid.email_templates())
-        );
-
-        for template in &config.sendgrid.email_templates() {
-            trace!(
-                logger,
-                "template: {}, required_fields: {:?}",
-                template,
-                config
-                    .sendgrid
-                    .required_fields_for_email(template, logger)
-                    .unwrap()
-            )
-        }
-
-        Ok(config)
+        Ok(serde_yaml::from_str(&buf).context("parsing the YAML configuration")?)
     }
 }
