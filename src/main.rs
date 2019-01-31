d5857cb8054c244c84b14dbcd22f497a274a9213
Otavio Salvador <otavio@ossystems.com.br>
2019-01-31 17:59:40 -0200

    Revert "Allow use with Rust 1.31.1"
    This reverts commit 26f9c056acb21f82f877fdfd3e9fceb22c6b1ad6.
    
    Rust 1.32.0 has been released and `FromStr` is now implemented for
    `PathBuf` so we can now revert this change.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ mod sendgrid;
 #[derive(StructOpt, Debug)]
 struct Cli {
     /// Configuration file to use
-    #[structopt(short = "c", long = "config", parse(from_os_str))]
+    #[structopt(short = "c", long = "config")]
     config: PathBuf,
     /// Increase the verboseness level
     #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
